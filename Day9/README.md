bb12696b1aca2bdd91a6346fb3da48d38b1ba29f
tolash123 <idreesy31@gmail.com>
2023-09-06 11:26:22 +0100

    day9

---
+++ Day9/README.md
@@ -0,0 +1,22 @@
+# Day 9
+
+Types and Numbers in JS
+
+## Js is divided into two types:
++ primitive types:
+ + numbers
+ + text or string
+ + Boolean
+ + null
+ + undefined 
+
++ Object types:
+ - global object
+ - array
+ - function
+ - Classes & Constructors
+ - garbage collection
+
+## Javascript Numbers
+
+It has only one type of number. Numbers can be written with or without decimals.
---
+++ Day9/day9.js
@@ -0,0 +1,18 @@
+Math.pow(2,53) // function in JavaScript is used to power of a number
+Math.round(.6) // function returns the value of a number rounded to the nearest integer.
+console.log(Math.ceil(.6)) // round a number upward to the nearest integer
+console.log(Math.floor(.6)) //rounds a number downwards to the nearest integer
+console.log(Math.abs(-5)) // return the absolute value of a number
+Math.max(5,6,8) //return the largest numbers
+Math.min(5,6,2) //return the lowest-valued number
+Math.random() // x where 0 <= x < 1.0
+Math.PI // π: circumference of a circle / diameter
+Math.E // e: The base of the natural logarithm
+Math.sqrt(3) // The square root of 3
+Math.pow(3, 1/3) // The cube root of 3
+Math.sin(0) // also Math.cos, Math.atan, etc.
+Math.log(10) // Natural logarithm of 10
+Math.log(100)/Math.LN10 // Base 10 logarithm of 100 
+Math.log(512)/Math.LN2 // Base 2 logarithm of 512 
+console.log(Math.exp(3)) // Math.E cubed // use the console.log() while using node to run the file
+
