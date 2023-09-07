c756513c9a90914cfe5af2be1c1edcfaf1f5a5e9
tolash123 <idreesy31@gmail.com>
2023-09-07 13:50:35 +0100

    day23

---
+++ Day23/day23.js
@@ -0,0 +1,32 @@
+var i =0;
+HowToLabel: while (i<3){
+    console.log("i= " +i+ "\n");
+    i++;
+}
+
+console.log("\n")
+
+for(var i = 0; i < 1000; i++){
+    console.log("Hello \n");
+    if(i==2)
+    break;
+}
+
+console.log("\n")
+
+var x = 0;
+var z = 0;
+labelCancelLoops: while(true){
+    console.log("Outer loops(x):" + x + " ");
+    x += 1;
+    z = 1;
+    while(true){
+        console.log("Inner loops(z):" + x + "\n");
+        z += 1;
+        if (z === 3 && x === 3){
+            break labelCancelLoops;
+        }else if (z === 3){
+            break;
+        }
+    }
+}
