84940a9bbaca2ced7fe665809bd206084bba206f
tolash123 <idreesy31@gmail.com>
2023-09-11 16:00:07 +0100

    day46&47

---
+++ Day46&47/README.md
@@ -0,0 +1,5 @@
+# Day 46&47
+
+Using Arrays
+
++ Create Array to store students name and Array to store GPA of students 'double' then assign each student name with his GPA
---
+++ Day46&47/weekend.js
@@ -0,0 +1,9 @@
+var studentName = ['Alice', 'Dristol', 'Barkley', 'Ojo', 'simeon', 'Joseph'];
+var GPA = [2.0, 3.4, 4.6, 4.2, 3.9, 4.4];
+
+for (var i = 0; i<studentName.length; i++){
+   console.log(studentName[i] +" "+ GPA[i]);
+  /*document.write("<tr><td>" + studentName[i] + "</td>");
+  document.write("td" + GPA[i] + "</td></tr>")
+*/
+}
