16534da15624bf6440eb065c2cb19b91418205fa
tolash123 <idreesy31@gmail.com>
2023-09-12 13:41:13 +0100

    day56

---
+++ Day56/day56.js
@@ -0,0 +1,16 @@
+var numbers = [1, 2, 3, 4, 5];
+
+//splice
+console.log(numbers.splice(1,3) + "\n");
+//push
+numbers.push(6);
+console.log(numbers + "\n");
+//pop
+numbers.pop();
+console.log(numbers + "\n");
+//unshift
+numbers.unshift(7);
+console.log(numbers + "\n");
+//shift
+numbers.shift();
+console.log(numbers + '\n');
