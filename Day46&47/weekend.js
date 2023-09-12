84b11fbf8661f8016d29ec3f8e225ab026af026a
tolash123 <idreesy31@gmail.com>
2023-09-12 12:34:24 +0100

    day51

--- Day46&47/weekend.js
+++ Day46&47/weekend.js
@@ -4,6 +4,6 @@ var GPA = [2.0, 3.4, 4.6, 4.2, 3.9, 4.4];
 for (var i = 0; i<studentName.length; i++){
    console.log(studentName[i] +" "+ GPA[i]);
   /*document.write("<tr><td>" + studentName[i] + "</td>");
-  document.write("td" + GPA[i] + "</td></tr>")
+  document.write("<td>" + GPA[i] + "</td></tr>")
 */
 }
---
+++ Day51/day51.js
@@ -0,0 +1,7 @@
+
+var txt = "";
+var numbers = [45, 6, 8, 25];
+for (var i = 0; i<numbers.length; i++){
+    txt = txt + numbers[i];
+}
+console.log(txt);
