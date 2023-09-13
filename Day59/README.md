72d09d0b66b202916361e04124a4d4aa79ae8388
tolash123 <idreesy31@gmail.com>
2023-09-13 21:41:53 +0100

    day59

---
+++ Day59/README.md
@@ -0,0 +1,9 @@
+# Day 59
+
+### Strings as Arrays
+
+- charAt(): returns the character at the specified index(position)
+- include(): checks whether a string/characters
+- localeCampare(): compares two strings in the current locale
+- toUpperCase(): converts a string to uppercase letters
+- toLowerCase(): converts a string to lowercase letters
---
+++ Day59/day59.js
@@ -0,0 +1,29 @@
+var str = 'Hello world!'
+//charAt()
+
+var a = str.charAt(0);
+var b = str.charAt(str.length-3);
+console.log(a + "\n");
+console.log(b + "\n")
+
+//includes
+var n = str.includes('world');
+console.log(n + "\n")
+
+//localCompare
+var str1 = 'ab'
+var str2 = 'ab'
+var n = str1.localeCompare(str2);
+console.log(n + '\n')
+
+//search
+var d = str.search("Hello");
+console.log(d + "\n");
+
+//toUpperCase
+var d = str.toUpperCase();
+console.log(d + "\n");
+
+//toLowerCase
+var d = str.toLowerCase();
+console.log(d + "\n")
